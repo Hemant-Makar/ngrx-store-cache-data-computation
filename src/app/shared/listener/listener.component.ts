2c1bd79f2eeb0ac9e9a1432f3fa43ff95147a508
Hemant-Makar <hemant.makar@gmail.com>
2021-05-14 19:45:41 +0530

    integrated plotly

--- src/app/shared/listener/listener.component.ts
+++ src/app/shared/listener/listener.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IMonitorData } from '../../models/imonitor-data';
 import { StoreDataService } from '../../ngrx-store/store-data.service';
-
+declare const Plotly;
 @Component({
   selector: 'app-listener',
   templateUrl: './listener.component.html',
@@ -11,10 +11,33 @@ import { StoreDataService } from '../../ngrx-store/store-data.service';
 export class ListenerComponent implements OnInit {
   @Input()
   key: string;
-
-  public cacheObservable$: Observable<IMonitorData[]>;
+  public cacheObservable$: Observable<
+    IMonitorData[]
+  > = this.storeService.getWidgetCacheData('A');
   public dataObservable$: Observable<IMonitorData>;
   public countObservable$: Observable<number>;
+
+  public graph = {
+    data: [
+      {
+        x: [],
+        y: [],
+        type: 'line'
+      }
+    ],
+    layout: {
+      width: 420,
+      height: 240,
+      xaxis: {
+        range: [],
+        // visible: false
+        showticklabels: false
+      },
+      yaxis: {
+        range: [0, 10]
+      }
+    }
+  };
 
   constructor(private storeService: StoreDataService) {}
 
@@ -22,5 +45,15 @@ export class ListenerComponent implements OnInit {
     this.countObservable$ = this.storeService.getCount();
     this.dataObservable$ = this.storeService.getWidgetData(this.key);
     this.cacheObservable$ = this.storeService.getWidgetCacheData(this.key);
+    this.cacheObservable$.subscribe(widgetCache => {
+      this.graph.data[0].x = widgetCache.map(cache => cache.time);
+      this.graph.data[0].y = widgetCache.map(cache => cache.value);
+      const maxRange = this.graph.data[0].x.pop();
+      this.graph.data[0].x.push(maxRange);
+      const minRange = maxRange - 60000;
+      console.log(maxRange, minRange);
+      this.graph.layout.xaxis.range = [minRange, maxRange];
+      Plotly.react(this.key, this.graph.data, this.graph.layout);
+    });
   }
 }
